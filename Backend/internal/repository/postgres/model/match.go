114a1a21ee38c6b7fca010bb52c492161bee903a
leonidlivshits <Leolivshitsss@yandex.ru>
2025-04-24 00:51:10 +0300

    Tables created

---
+++ Backend/internal/repository/postgres/model/match.go
@@ -0,0 +1,14 @@
+package model
+
+type Match struct {
+    ID          int     `gorm:"primaryKey"`
+    GroupID     *int
+    PlayoffID   *int
+    Team1ID     int
+    Team2ID     int
+    Score1      int
+    Score2      int
+    StageID      *int
+    NextMatchID *int
+    IsCompleted bool
+}
